4ddfe2f0cddb260936f18c651fe3731ae304bac2
章博 <zhangbo@jiagouyun.com>
2023-02-21 18:37:33 +0800

    修改日志，修改页面描述

--- Hbuilder_Example/pages/rum/rum-view-mixin.vue
+++ Hbuilder_Example/pages/rum/rum-view-mixin.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<text>页面 mixin 方式实现 rum view，注意需要与 gc_watchRouter 分开使用否则会重复上报 view 数据</text>
+		<text>页面 mixin 方式实现 rum view，注意需要与 GCWatchRouter 分开使用否则会重复上报 view 数据</text>
 		
 	</view>
 </template>
