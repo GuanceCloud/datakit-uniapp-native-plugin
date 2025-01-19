07db9d30b504eb6def0667f309a6dbfa6d6ce4fe
hulilei <huuuu1016@gmail.com>
2025-01-19 20:34:01 +0800

    添加 gc.request 方法进行 resource 数据采集与追踪

---
+++ Hbuilder_Example/GCRequest.js
@@ -0,0 +1,91 @@
+var rum = uni.requireNativePlugin("GCUniPlugin-RUM");
+var tracer = uni.requireNativePlugin("GCUniPlugin-Tracer");
+// 获取平台信息
+const platform = uni.getSystemInfoSync().platform;
+
+export default {
+    getUUID() {
+    	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    		var r = Math.random() * 16 | 0,
+    			v = c == 'x' ? r : (r & 0x3 | 0x8);
+    		return v.toString(16);
+    	});
+    },
+    isEmpty(value) {
+	    return value === null || value === undefined;
+	},
+	/// 通过 filterPlatform 参数进行平台过滤，当开启 `enableNativeUserResource` 时，
+	//  由于 uniapp 的网络请求在 iOS 端是使用系统 API 实现的，iOS 所有 resource 数据能够一并采集，
+	/// 此时请屏蔽 iOS 端 uniapp 中手动采集，以防止数据重复采集。
+	/// 例:["ios"], iOS 端设置不进行 trace 追踪与 RUM 采集。
+    request(options){ 
+		let key = this.getUUID();
+		var filter;
+		if(this.isEmpty(options.filterPlatform)){
+			filter = false
+		}else{
+	        filter = options.filterPlatform.includes(platform);
+		}
+		var traceHeader = {}
+		if(filter == false){
+		  // trace 关联 RUM
+		  var traceHeader = tracer.getTraceHeader({
+		  	'key': key,
+		  	'url': options.url,
+		  })
+		}
+		traceHeader = Object.assign({},traceHeader, options.header)
+		rum.startResource({
+			'key':key,
+		});
+		var responseHeader;
+		var responseBody;
+		var resourceStatus;
+		return uni.request({
+			url: options.url,
+			method: options.method,
+			header: traceHeader,
+			data:options.data,
+			timeout:options.timeout,
+			success: (res) => {
+				if(filter){
+				  responseHeader = res.header;
+				  responseBody = res.data;
+				  resourceStatus = res.statusCode;
+				}
+				if(this.isEmpty(options.success)){
+					options.success(res);
+				}
+			},
+			fail:(err) => {
+				if(!filter){
+				  responseBody = err.errMsg;
+				}
+				if(this.isEmpty(options.fail)){
+					options.fail(err);
+				}
+			},
+			complete() {
+				if(!filter){
+				  rum.stopResource({
+				  	'key':key,
+				  })
+				  rum.addResource({
+				  	'key': key,
+				  	'content': {
+				  		'url': options.url,
+				  		'httpMethod': options.method,
+				  		'requestHeader': traceHeader,
+				  		'responseHeader': responseHeader,
+				  		'responseBody': responseBody,
+				  		'resourceStatus': resourceStatus,
+				  	}
+				  })
+				}
+				if(this.isEmpty(options.complete)){
+					options.complete();
+				}
+			}
+		});
+	}
+} 
--- Hbuilder_Example/utils.js
+++ Hbuilder_Example/utils.js
@@ -1,9 +1,8 @@
-var rum = uni.requireNativePlugin("GCUniPlugin-RUM");
 export const SERVER_URL = 'http://127.0.0.1:9529'
 export const ANDROID_APP_ID = 'android_app_id'
 export const IOS_APP_ID = 'ios_app_id'
 export const TRACK_ID = 'track_id'
-
+import gc from './GCRequest.js';
 export default {
 
 	getUUID() {
@@ -14,7 +13,7 @@ export default {
 		});
 	},
 	request(requestUrl, header = {}) {
-		uni.request({
+		gc.request({
 			url: requestUrl,
 			header: header,
 			success() {
@@ -25,53 +24,21 @@ export default {
 			}
 		});
 	},
-	rumRequest(requestUrl,method,header = {}){
-		    let key = this.getUUID();
-			var traceHeader = tracer.getTraceHeader({
-				'key': key,
-				'url': requestUrl,
-			})
-			traceHeader = Object.assign({},traceHeader, header)
-			rum.startResource({
-				'key':key,
-				'property':{
-					'startResource_property':'uni_test'
-				}
-			});
-			var responseHeader;
-			var responseBody;
-			var resourceStatus;
-			uni.request({
+	rumRequest(requestUrl,method,header = {}){			
+			gc.request({
 				url: requestUrl,
 				method: method,
-				header: traceHeader,
-				success: (res) => {
-					console.log('调用getTraceHeader:' + JSON.stringify(header))
-					responseHeader = res.header;
-					responseBody = res.data;
-					resourceStatus = res.statusCode;
+				header: header,
+				filterPlatform:["ios"],
+				timeout:30000,
+				success(res)  {
+					console.log('success:' + JSON.stringify(res))
 				},
-				fail: (err) =>{
-					responseBody = err.errMsg;
+				fail(err) {
+					console.log('fail:' + JSON.stringify(err))
 				},
 				complete() {
-					rum.stopResource({
-						'key':key,
-						'property':{
-							'stopResource_property':'uni_test'
-						}
-					})
-					rum.addResource({
-						'key': key,
-						'content': {
-							'url': requestUrl,
-							'httpMethod': method,
-							'requestHeader': header,
-							'responseHeader': responseHeader,
-							'responseBody': responseBody,
-							'resourceStatus': resourceStatus,
-						}
-					})
+					console.log('complete:' + JSON.stringify(err))
 				}
 			});
 	}
