0d606a08f3c7143e286fb5124814417cae5c97dc
hulilei <huuuu1016@gmail.com>
2025-07-17 19:15:25 +0800

    update GC_UniPlugin_App.xcframework 0.2.3

---
+++ Hbuilder_Example/nativeplugins/GCUniPlugin/ios/GC_UniPlugin_App.xcframework/ios-arm64/GC_UniPlugin_App.framework/Headers/FTLogModule.h
@@ -0,0 +1,16 @@
+//
+//  FTLogModule.h
+//  GC-UniPlugin-App
+//
+//  Created by hulilei on 2023/2/3.
+//
+
+#import "DCUniModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTLogModule : DCUniModule
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hbuilder_Example/nativeplugins/GCUniPlugin/ios/GC_UniPlugin_App.xcframework/ios-arm64/GC_UniPlugin_App.framework/Headers/GC-UniPlugin-App-Version.h
@@ -0,0 +1,13 @@
+//
+//  GC_UniPlugin_App_Version.h
+//  GC-UniPlugin-App
+//
+//  Created by hulilei on 2023/10/7.
+//
+
+#ifndef GC_UniPlugin_App_Version_h
+#define GC_UniPlugin_App_Version_h
+
+#define UniPluginAppVersion @"0.2.3"
+
+#endif /* GC_UniPlugin_App_Version_h */
