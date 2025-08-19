bd6fa3c9f57adef1dd473009a3ed3423d0ac13fd
hulilei <huuuu1016@gmail.com>
2025-08-19 11:29:40 +0800

    replace `this.route` with `getCurrentPages()` to achieve the best compatibility.

--- Hbuilder_Example/GCPageMixin.js
+++ Hbuilder_Example/GCPageMixin.js
@@ -3,29 +3,42 @@
  */
 var rum = uni.requireNativePlugin("GCUniPlugin-RUM");
 var loadStart;
+
+/**
+ * Get the current page route path 
+ * @returns {string|null} The current page route (e.g. "pages/home/index"), returns null if failed
+ */
+const getCurrentPagePath = () => {
+  try {
+    return getCurrentPages().pop()?.route || null;
+  } catch (e) {
+    return null;
+  }
+};
+
 export default {
 	onLoad() {
 		loadStart = new Date().getTime() * 1000000
-		//console.log('onLoad');
 	},
 	onReady() {
-		//console.log('onReady');
 		if (getApp().globalData.GCFirstLoad) {
 			getApp().globalData.GCFirstLoad = false
 			const loadEnd = new Date().getTime() * 1000000
 			let duration = (loadEnd - loadStart);
-			if (loadStart !== null && duration >= 0) {
+			let pagePath = getCurrentPagePath();
+			if (pagePath === null){
+				return;
+			}
+			if (loadStart !== null && duration >= 0 ) {
 				rum.onCreateView({
-					'viewName': this.route,
+					'viewName': pagePath,
 					'loadTime': duration,
 				})
 			}
 			loadStart = null;
 			rum.startView({
-				'viewName': this.route
+				'viewName': pagePath
 			})
 		}
 	}
-
-
 }
--- Hbuilder_Example/GCPageViewMixinOnly.js
+++ Hbuilder_Example/GCPageViewMixinOnly.js
@@ -3,35 +3,59 @@
  */
 var rum = uni.requireNativePlugin("GCUniPlugin-RUM");
 var loadStart;
+
+/**
+ * Get the current page route path 
+ * @returns {string|null} The current page route (e.g. "pages/home/index"), returns null if failed
+ */
+const getCurrentPagePath = () => {
+  try {
+    return getCurrentPages().pop()?.route || null;
+  } catch (e) {
+    return null;
+  }
+};
+
 export const rumViewMixin = {
 	onLoad() {
 		loadStart = new Date().getTime() * 1000000
-		console.log('onLoad');
 	},
 	onReady() {
+		// The page has completed its initial rendering.
 		const loadEnd = new Date().getTime() * 1000000
-		let duration = (loadEnd - loadStart);
-		if (loadStart !== null && duration >= 0) {
+		var duration = 0;
+		if (loadStart !== null) {
+			duration = (loadEnd - loadStart);
+		}
+		loadStart = null;
+		let pagePath = getCurrentPagePath();
+		if (pagePath === null){
+			return;
+		}
+		if (duration >= 0 ) {
 			rum.onCreateView({
-				'viewName': this.route,
+				'viewName': pagePath,
 				'loadTime': duration,
 			})
 		}
-		loadStart = null;
 		rum.startView({
-			'viewName': this.route
+			'viewName': pagePath
 		})
 
 	},
 	onShow() {
-		if (loadStart != null) {
+		// The page is triggered each time it appears on the screen.
+		if (loadStart === null) {
+			let pagePath = getCurrentPagePath();
+			if (pagePath !== null){
 			rum.startView({
-				'viewName': this.route
+				'viewName': pagePath
 			})
+			}
 		}
 	},
-	onHide() {
+	onUnload() {
 		rum.stopView()
-	},
+	}
 
 }
