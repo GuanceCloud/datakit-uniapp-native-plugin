95aa7b2d044b6510ef36b7e4a9f511322090630f
hulilei <huuuu1016@gmail.com>
2025-09-12 17:46:34 +0800

    update version to 0.2.4

--- UniPlugin-iOS/HBuilder-uniPluginDemo/GC-UniPlugin-App/GC-UniPlugin-App/Classes/GC-UniPlugin-App-Version.h
+++ UniPlugin-iOS/HBuilder-uniPluginDemo/GC-UniPlugin-App/GC-UniPlugin-App/Classes/GC-UniPlugin-App-Version.h
@@ -8,6 +8,6 @@
 #ifndef GC_UniPlugin_App_Version_h
 #define GC_UniPlugin_App_Version_h
 
-#define UniPluginAppVersion @"0.2.4-alpha.3"
+#define UniPluginAppVersion @"0.2.4"
 
 #endif /* GC_UniPlugin_App_Version_h */
